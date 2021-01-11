9a8e08b56cd3b69f3dd84ecc0fb28dc3186ae75a
Noah Christian <noahchristian@Noahs-MacBook-Pro.local>
2021-01-10 19:47:25 -0500

    fixed login and remember me

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -25,9 +25,18 @@ end
 def logged_in?
     !current_user.nil?
 end
+
+def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+end
 # Logs out the current user.
 def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
-    end
+end
+
+
 end
