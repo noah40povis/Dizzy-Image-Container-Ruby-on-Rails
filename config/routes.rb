5dd3d3536e2c51282ba33912b2259b257b0cfb0a
Noah Christian <noahchristian@Noahs-MacBook-Pro.local>
2021-01-16 11:05:51 -0500

    edited nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :pictures
   root 'users#new'
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users
+  get '/picture/new', to: 'pictures#new'
+  resources :users, :pictures
 end
